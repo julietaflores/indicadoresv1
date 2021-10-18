029a75a61a2b296fd0068b4a307144a623fc29e0
DESKTOP-7JILCRN\dell <julietita16>
2021-10-18 17:17:45 -0400

    act_v1

--- src/app/models/dataIndicador.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface DataIndicador{
-    anioActual:Number|undefined,
-    mesActual: String | undefined ,
-    monedaActual: Number | undefined 
- }
--- src/app/models/userAuth.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface UserAuth{
-   idUsuario:Number|undefined,
-   name: String | undefined ,
-   password:any | undefined ,
-   idRol: Number | undefined,
-   language:any|String|undefined|null
-}

924de9de4af667ba8a9c09618b90a59ccf671c6b
DESKTOP-7JILCRN\dell <julietita16>
2021-10-28 14:59:36 -0400

    fg1

--- src/app/models/indicadorVentas.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IndicadorVentas{
-    Monto_Mes:any;
-    Monto_Acumulado:any;
-    P_Monto_Mes:any;
-    P_Monto_Acumulado:any;
-}
--- src/app/models/pageinfoo.ts
+++
@@ -1,4 +0,0 @@
-export interface pageinf{
-   title: String | undefined ,
-  
-}
